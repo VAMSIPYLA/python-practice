b43cfac43e65becaf17749c06a658c43c8f8d672
VAMSIPYLA <vamsiravindra.vr@gmail.com>
2020-11-24 10:15:39 +0530

    coditional stmts hackerrank

---
+++ if_elif.py
@@ -0,0 +1,17 @@
+#If n is odd, print Weird
+#If n is even and in the inclusive range of 2  to 5, print Not Weird
+#If n is even and in the inclusive range of 6 to 20, print Weird
+#If n is even and greater than 20 , print Not Weird
+
+
+
+N = int(input())
+
+if N % 2 == 1:
+    print("Weird")
+elif N % 2 == 0 and range(1, 6):
+    print("Not Weird")
+elif N % 2 == 0 and range(5, 21):
+    print("Weird")
+elif N % 2 == 0 and N >= 20:
+    print("Not Weird")
